5e95e6da828d5cf110f93180a6dacfb651f5aeb4
wwsheng009 <vincentwwsheng@gmail.com>
2024-10-16 23:20:29 +0800

    add tools for generate codes

--- studio/tool.ts
+++ studio/tool.ts
@@ -1,7 +1,53 @@
 import { FindCachedModelById } from '@scripts/app/litemall/serivce/data';
 import { FS, http, Process } from '@yaoapps/client';
+import { YaoModel } from '@yaoapps/types';
 
 /**
+ * Yao缓存中的模型定义
+ */
+export interface CachedModel extends YaoModel.ModelDSL {
+  ID?: string;
+  DSL?: string;
+}
+
+export interface CachedModelTree {
+  children?: { data?: CachedModel }[];
+  data?: CachedModel;
+}
+/**
+ * 解析内存中的模型数据
+ * @param {*} modelData
+ * @returns
+ */
+export function modelIdListFromMemory(modelData: CachedModelTree) {
+  let idList = [];
+  if (modelData.children) {
+    modelData.children.forEach((line) => {
+      const subLine = modelIdListFromMemory(line);
+      idList = idList.concat(subLine);
+    });
+  } else if (modelData.data) {
+    idList.push(modelData.data.ID);
+  } else if (Array.isArray(modelData)) {
+    modelData.forEach((line) => {
+      const subLine = modelIdListFromMemory(line);
+      idList = idList.concat(subLine);
+    });
+  }
+  return idList;
+}
+/**
+ * yao studio run tool.getCachedModelIDList
+ * @returns
+ */
+export function getCachedModelIDList(): string[] {
+  const models = Process('widget.models');
+
+  return modelIdListFromMemory(models);
+}
+/**
+ * use the yao-amis-admin service to generate the code
+ *
  * 发起YAO-AMIS-ADMIN远程调用的函数
  *
  * @param {string} remoteMethod - 要调用的远程服务的处理器
@@ -28,7 +74,6 @@ function remoteCall(remoteMethod, ...args) {
     null,
     { Authorization: `Bearer ${amisAdminKey}` }
   );
-  console.log(response);
   if (response.status != 200 || response.data.code != 200) {
     throw new Error(response.message);
   }
@@ -59,10 +104,24 @@ export function generateModelCode(modelId) {
     modelDsl
   );
 
-  const fname = toCamelCaseNameSpace(modelId);
-  const fs = new FS('script');
+  const serviceName = toCamelCaseNameSpace(modelId);
 
-  fs.WriteFile(`/models/${fname}Service.ts`, code);
+  const fname = `/db_services/${serviceName}Service.ts`;
+  const fs = new FS('script');
+  if (!fs.Exists(fname)) {
+    fs.WriteFile(fname, code);
+    console.log(`create file: ${fname}`);
+  } else {
+    console.log(`file ${fname} already exists`);
+  }
+}
+
+/**
+ * 生成所有模型服务代码
+ * yao studio run tool.generateAllModelService
+ */
+export function generateAllModelService() {
+  getCachedModelIDList().map(generateModelCode);
 }
 
 /**
@@ -74,13 +133,17 @@ export function generateModelCode(modelId) {
 export function generateModelTypeCode(modelId) {
   const modelDsl = FindCachedModelById(modelId);
 
-  const code = remoteCall('scripts.system.tstype.createTSTypes', modelDsl);
+  const code = remoteCall('scripts.system.tstype.createTSTypes', modelDsl, 'I');
 
-  const fname = toCamelCaseNameSpace(modelId);
+  const typeName = toCamelCaseNameSpace(modelId);
+  const fname = `/db_services/I${typeName}.ts`;
   const fs = new FS('script');
-
-  fs.WriteFile(`/models/I${fname}.ts`, code);
+  if (!fs.Exists(fname)) {
+    fs.WriteFile(fname, code);
+    console.log(`create file: ${fname}`);
+  } else {
+    console.log(`file ${fname} already exists`);
+  }
 }
-
 // const code = generateModelCode('admin.user');
 // console.log(code);
